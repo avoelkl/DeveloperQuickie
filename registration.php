64e8b0a23c8b5fbdd373559b5921431b223be089
Anna Völkl <a.voelkl@limesoda.com>
2016-07-11 13:24:52 +0200

    Initial commit

---
+++ registration.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Avoelkl_DeveloperQuickie extension
+ *
+ * @package   Avoelkl_DeveloperQuickie
+ * @copyright 2016 Anna Völkl
+ * @license   OSL-3.0 - See LICENSE.md for license details.
+ * @author    Anna Völkl <a.voelkl@limesoda.com>
+ */
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'Avoelkl_DeveloperQuickie',
+    __DIR__
+);
